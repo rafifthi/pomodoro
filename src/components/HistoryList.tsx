8be0d3db24259c70772466a2b0e8a1f8bcc35966
Rafif Fathi Misbah <rafifthii@gmail.com>
2025-05-21 13:36:25 +0700

    Empty state for History list

--- src/components/HistoryList.tsx
+++ src/components/HistoryList.tsx
@@ -2,7 +2,6 @@
 
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { History } from "lucide-react"; // atau ilustrasi lain
 
 
 export type HistoryItem = {
